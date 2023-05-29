3e902064859c6d9eb1f7748cbc6f807e07fdf702
Luka Kresoja <57074484+LukeOnuke@users.noreply.github.com>
2023-05-30 01:21:07 +0200

    Update gameMain.mjs

--- js/game/gameMain.mjs
+++ js/game/gameMain.mjs
@@ -2,7 +2,7 @@ import {KEYCODE_A, KEYCODE_D, KEYCODE_ESCAPE, KEYCODE_S, KEYCODE_SPACE} from "./
 import {addElementToGameField, createBullet, createInvader} from "./objects.mjs";
 import {convertRemToPixels, getPxNumber, isColliding, moveX, moveY} from "./physics.mjs";
 import {EXPLOSION, LASER_SHOOT, playSound, playSoundRepeating} from "./audio.mjs";
-import {getHighScore, incrementHighScore, solidifyHighScore} from "./state.mjs";
+import {getHighScore, incrementHighScore, resetTempHighScore, solidifyHighScore} from "./state.mjs";
 
 let keyPressedArray = [];
 const player = document.getElementById("player");
@@ -112,11 +112,11 @@ let heatDamage = 0;
 function die() {
     window.location.replace("/dead");
     solidifyHighScore();
+    resetTempHighScore();
 }
 
 function victory() {
     window.location.replace(`/game/?${invaderSpawnAmount + 1}-${fireSpeed * 2 / 3}`);
-    solidifyHighScore();
 }
 
 function incrementAndDisplayHighScore(delta){
